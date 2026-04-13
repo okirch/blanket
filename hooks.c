79343ee8f81a646b82f1b0e109a5a1ab1f072f6d
Olaf Kirch <okir@suse.com>
2026-04-13 12:49:40 +0200

    Add hook to capture thread creation
    Signed-off-by: Olaf Kirch <okir@suse.com>

--- hooks.c
+++ hooks.c
@@ -1,8 +1,12 @@
+/*
+ * Various hooks that let us enable sampling as needed/requests.
+ */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include <dlfcn.h>
+#include <pthread.h>
 
 #include "blanket.h"
 
@@ -73,3 +77,67 @@ dlopen(const char *path, int flags)
 
 	return h;
 }
+
+/*
+ * Intercept calls to pthread_create
+ *
+ * Right now, this will work *only* if we already decided to enable sampling before
+ * the process calls pthread_create(). In most cases, this will be fine, but things
+ * may be different when the application uses dlopen(), and we only discover during
+ * dlopen that we should enable PC sampling.
+ * Ignore this case for now...
+ */
+
+struct sc_pthread_args {
+	void *		(*start_routine)(void *);
+	void *		arg;
+};
+
+static void *
+start_thread_with_sampling(void *arg)
+{
+	struct sc_pthread_args *pargs = arg;
+	void *r;
+
+	/* If sampling is enabled, set up signal handler and timer for this new thread, too */
+	sc_sampling_activate_thread();
+
+	r = pargs->start_routine(pargs->arg);
+	free(pargs);
+	return r;
+}
+
+int
+pthread_create(pthread_t *restrict thread,
+		  const pthread_attr_t *attr,
+		  void *(*start_routine)(void *),
+		  void *arg)
+{
+	static int	(*__real_pthread_create)(pthread_t *restrict thread,
+				const pthread_attr_t *attr,
+				void *(*start_routine)(void *),
+				void *arg);
+	struct sc_pthread_args *pargs;
+
+	/* should we report an error in case dlsym() fails? */
+	if (__real_pthread_create == NULL) {
+		__real_pthread_create = dlsym(RTLD_NEXT, "pthread_create");
+		if (__real_pthread_create == NULL)
+			perror("dlsym(RTLD_NEXT, 'pthread_create')");
+		else if (__real_pthread_create == pthread_create) {
+			fprintf(stderr, "dlsym(RTLD_NEXT, 'pthread_create') returns hook address");
+			__real_pthread_create = NULL;
+		}
+	}
+
+	/* The args struct that we pass to __real_pthread_create() must refer to memory that
+	 * remains valid after we return from this function (because the spawned thread needs
+	 * to access it). Hence, stack variables are not an option.
+	 */
+	pargs = calloc(1, sizeof(*pargs));
+	pargs->start_routine = start_routine;
+	pargs->arg = arg;
+
+	return __real_pthread_create(thread, attr, start_thread_with_sampling, pargs);
+}
+
